299ed54c8191ee5d03f5c35a5f554e6b55074362
Vinicius <Vinicius@DESKTOP-5SQQP23>
2022-11-10 23:49:34 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraAPI/Controllers/CalculadoraController.cs
@@ -0,0 +1,50 @@
+﻿using CalculadoraAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpGet("Somar/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Somar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.somar();
+
+            return Ok(calc);
+        }
+
+        [HttpGet("Subtrair/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Subtrair(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.subtrair();
+
+            return Ok(calc);
+        }
+
+        [HttpGet("Multiplicar/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Multiplicar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.multiplicar();
+
+            return Ok(calc);
+        }
+
+        [HttpGet("Dividir/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Dividir(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.dividir();
+
+            return Ok(calc);
+        }
+        
+
+
+    }
+}
---
+++ CalculadoraAPI/Models/Calculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraAPI.Models
+{
+    public class Calculadora
+    {
+        public double Valor1 { get; set; }
+        public double Valor2 { get; set; }
+
+        public double HorasCurso { get; set; }
+
+        public double Resultado { get; set; }
+
+        public Calculadora()
+        {
+
+        }
+        public Calculadora(double valor1, double valor2, double resultado)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+            Resultado = resultado;
+        }
+
+        public Calculadora(double valor1, double valor2)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+
+        public void somar()
+        {
+            Resultado = Valor1 + Valor2; 
+        }
+        public void subtrair()
+        {
+            Resultado = Valor1 - Valor2;
+        }
+        public void dividir()
+        {
+            Resultado = Valor1 / Valor2;
+        }
+        public void multiplicar()
+        {
+            Resultado = Valor1 * Valor2;
+        }
+
+    }
+}
